15babc139e40410a1baf441fbdbc176c8980c911
jojox6 <dougx6@gmail.com>
2021-11-03 11:58:16 -0300

    Estrutura do projeto

---
+++ fundamentos-nodejs/src/index.js
@@ -0,0 +1,11 @@
+const { request, response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response)=>{
+    //return response.send("Hello World!");
+    return response.json({message: "Hello World Ignite!"});
+})
+
+app.listen(3333);
