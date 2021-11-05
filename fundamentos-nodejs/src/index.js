91541415647d8530a1891089eecc5b41871c310e
jojox6 <dougx6@gmail.com>
2021-11-05 02:20:28 -0300

    alterações no index.js

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,9 +3,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response)=>{
+app.get("/cursos", (request, response)=>{
     //return response.send("Hello World!");
-    return response.json({message: "Hello World Ignite - Fundamentos NodeJS"});
-})
+    return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+app.post("/cursos", (request, response) =>{
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
 
+app.put("/cursos/:id", (request, response) =>{
+    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+
+});
+
+app.patch("/cursos/:id", (request, response) =>{
+    return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/cursos/:id", (request, response) =>{
+    return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+});
 app.listen(3333);
